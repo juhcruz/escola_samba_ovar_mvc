2595ddf3cf0342c75d2db35b6f6ce49e326f8fdf
Ju Cruz <137825492+juhcruz@users.noreply.github.com>
2026-09-23 16:45:28 +0100

    Update Database.php
    update

--- config/Database.php
+++ config/Database.php
@@ -1,26 +1,20 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "escola_samba_ovar_mvc";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+private $host = "localhost";
+private $db_name = "escola_samba_ovar_mvc";
+private $username = "root";
+private $password = "";
+public $conn;
 
-    public function getConnection() {
-        $this->conn = null;
-
-        try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4",
-                $this->username,
-                $this->password
-            );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $exception) {
-            echo "Erro de conexão: " . $exception->getMessage();
-        }
-
-        return $this->conn;
-    }
+public function getConnection() {
+$this->conn = null;
+try {
+$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4", $this->username, $this->password);
+$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $exception) {
+echo "Erro de conexão: " . $exception->getMessage();
+}
+return $this->conn;
+}
 }
 ?>
