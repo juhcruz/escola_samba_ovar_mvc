67c24e835718a90d4fa6b01ac6bcfe160fbbc3a2
Ju Cruz <137825492+juhcruz@users.noreply.github.com>
2026-09-23 16:16:15 +0100

    login e registo update
    update

--- views/login.php
+++ views/login.php
@@ -2,25 +2,25 @@
 <html lang="pt-PT">
 <head>
     <meta charset="UTF-8">
-    <title>Login - Área Privada</title>
+    <title>Login - Escola de Samba de Ovar</title>
     <link rel="stylesheet" href="css/main_styles.css">
 </head>
 <body>
-    <h1>Autenticação</h1>
+    <h1>Carnaval de Ovar - Autenticação</h1>
 
     <?php if (isset($erro)): ?>
         <p style="color: red;"><?= htmlspecialchars($erro) ?></p>
     <?php endif; ?>
 
     <form action="index.php?acao=login" method="POST">
-        <label>Utilizador:</label><br>
+        <label>Utilizador / E-mail:</label><br>
         <input type="text" name="utilizador" required style="width: 30%;"><br><br>
 
         <label>Palavra-passe:</label><br>
-<input type="password" name="palavra_passe" required placeholder="Digite a palavra-passe"><br><br>
+        <input type="password" name="palavra_passe" required placeholder="Digite a palavra-passe"><br><br>
 
         <button type="submit">Entrar</button>
-        <a href="index.php">Voltar</a>
+        <a href="index.php?acao=registo">Ainda não tem conta? Registar</a>
     </form>
 </body>
 </html>
---
+++ views/registo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+<head>
+    <meta charset="UTF-8">
+    <title>Registo - Escola de Samba de Ovar</title>
+    <link rel="stylesheet" href="css/main_styles.css">
+</head>
+<body>
+    <h1>Carnaval de Ovar - Novo Registo</h1>
+
+    <?php if (isset($erro)): ?>
+        <p style="color: red;"><?= htmlspecialchars($erro) ?></p>
+    <?php endif; ?>
+
+    <form action="index.php?acao=registo" method="POST">
+        <label>Nome Completo:</label><br>
+        <input type="text" name="nome" required style="width: 30%;"><br><br>
+
+        <label>Utilizador / E-mail:</label><br>
+        <input type="text" name="utilizador" required style="width: 30%;"><br><br>
+
+        <label>Palavra-passe:</label><br>
+        <input type="password" name="palavra_passe" required placeholder="Crie uma palavra-passe"><br><br>
+
+        <button type="submit">Registar</button>
+        <a href="index.php?acao=login">Voltar ao Login</a>
+    </form>
+</body>
+</html>
